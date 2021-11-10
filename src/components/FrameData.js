38182ef8299d41b64dd6a1309b690d79832a1f06
palpatine20 <20diobrando@gmail.com>
2021-11-10 16:56:50 +0700

    init

---
+++ src/components/FrameData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FrameData = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FrameData
---
+++ src/components/StatData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatData = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default StatData
